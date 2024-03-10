446ae5827ec0b2a84df20b9c38fb8c4e4715d5d9
NIKITA NEGI <130778536+nikita3006@users.noreply.github.com>
2024-03-10 14:32:29 +0530

    fixing build issue

--- src/Components/ReactQuery/UseQueryWrapper.tsx
+++ src/Components/ReactQuery/UseQueryWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { useQuery, QueryFunction, QueryKey } from '@tanstack/react-query';
 
 interface QueryWrapperProps<TData> {
